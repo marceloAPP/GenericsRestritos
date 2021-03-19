b68f38d6e5529a760038114e52280c3a889ad2a5
marceloAPP <marcelo_freeday@yahoo.com.br>
2021-03-18 22:55:21 -0300

    Implementando a classe produto com IComparable

--- Exemp_206/Entities/Produto.cs
+++ Exemp_206/Entities/Produto.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Exemp_206.Entities
 {
-    class Produto
+    class Produto : IComparable
     {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Produto))
+            {
+                throw new ArgumentException("Erro de Comparação: O argumento não é um produto");
+            }
+            Produto outro = obj as Produto;
+            return Preco.CompareTo(outro.Preco);
+        }
     }
 }
--- Exemp_206/Program.cs
+++ Exemp_206/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Exemp_206.Services;
+using Exemp_206.Entities;
 
 namespace Exemp_206
 {
@@ -8,21 +10,23 @@ namespace Exemp_206
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
+            List<Produto> list = new List<Produto>();
 
             Console.Write("Entre com Quantidade: ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
-                int x = int.Parse(Console.ReadLine());
-                list.Add(x);
+                string[] vect = Console.ReadLine().Split(',');
+                string nome = vect[0];
+                double preco = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Produto(nome, preco));
             }
             ServicoDeCalculo servicoCalculo = new ServicoDeCalculo();
 
-            int max = servicoCalculo.Max(list);
+            Produto max = servicoCalculo.Max(list);
 
-            Console.Write("\nMaximo: ");
+            Console.Write("\nMaior: ");
             Console.WriteLine(max);
 
         }
