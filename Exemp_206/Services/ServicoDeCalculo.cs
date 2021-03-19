3893e1664974ef64678ee200de25443ee1f03514
marceloAPP <marcelo_freeday@yahoo.com.br>
2021-03-18 22:09:18 -0300

    Implementando o servico de Calculo

---
+++ Exemp_206/Services/ServicoDeCalculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exemp_206.Services
+{
+    class ServicoDeCalculo
+    {
+        public int Max(List<int> list)
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("Lsita não pode estar vazia!");
+            }
+
+            int max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
