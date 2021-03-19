e9e17bbe5ee747b2d55b8a1173c066eee2051e37
marceloAPP <marcelo_freeday@yahoo.com.br>
2021-03-18 22:33:20 -0300

    Implementando o programa principal com IComparable na classe ServicoDeCalculo

--- Exemp_206/Program.cs
+++ Exemp_206/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Exemp_206.Services;
 
 namespace Exemp_206
 {
@@ -10,6 +8,23 @@ namespace Exemp_206
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
+
+            Console.Write("Entre com Quantidade: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            ServicoDeCalculo servicoCalculo = new ServicoDeCalculo();
+
+            int max = servicoCalculo.Max(list);
+
+            Console.Write("\nMaximo: ");
+            Console.WriteLine(max);
+
         }
     }
 }
--- Exemp_206/Services/ServicoDeCalculo.cs
+++ Exemp_206/Services/ServicoDeCalculo.cs
@@ -5,17 +5,18 @@ namespace Exemp_206.Services
 {
     class ServicoDeCalculo
     {
-        public int Max(List<int> list)
+        public  T Max<T>(List<T> list) where T : IComparable
         {
             if (list.Count == 0)
             {
                 throw new ArgumentException("Lsita não pode estar vazia!");
             }
 
-            int max = list[0];
+
+            T max = list[0];
             for (int i = 1; i < list.Count; i++)
             {
-                if (list[i] > max)
+                if (list[i].CompareTo(max) > 0)
                 {
                     max = list[i];
                 }
